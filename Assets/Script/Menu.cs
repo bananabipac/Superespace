7d5f5c17730d9ab51601b84a7fd3fea3870d6ab8
Romain <romain.pedra@bbox.fr>
2012-04-08 18:57:58 +0200

    Modifs menu

--- Assets/Script/Menu.cs
+++ Assets/Script/Menu.cs
@@ -18,5 +18,9 @@ public class Menu : MonoBehaviour {
 		if(GUI.Button(new Rect(Screen.width * (1f/6.55f),Screen.height * (0.2f/6.3f),Screen.width * (4.8f/6.55f),Screen.height * (0.85f/6.3f)),"Multiplayer")) {
 			Application.LoadLevel(1);
 		}
+		if(GUI.Button(new Rect(Screen.width * (1f/6.55f),10*Screen.height * (0.2f/6.3f),Screen.width * (4.8f/6.55f),Screen.height * (0.85f/6.3f)),"Exit")) {
+			Application.Quit();
+		}
+		
 	}
 }
