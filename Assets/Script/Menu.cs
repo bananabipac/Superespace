445da8407e7fd2e356838ec0f5f316d956136874
Romain <romain.pedra@bbox.fr>
2012-04-17 22:06:25 +0200

    plugin XFECT + MENU

--- Assets/Script/Menu.cs
+++ Assets/Script/Menu.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class Menu : MonoBehaviour {
-
+	public Font font;
 	// Use this for initialization
 	void Start () {
 	
@@ -14,11 +14,14 @@ public class Menu : MonoBehaviour {
 	}
 	
 	void OnGUI() {
-	
-		if(GUI.Button(new Rect(Screen.width * (1f/6.55f),Screen.height * (0.2f/6.3f),Screen.width * (4.8f/6.55f),Screen.height * (0.85f/6.3f)),"Multiplayer")) {
+		GUIStyle style = new GUIStyle();
+		style.alignment = TextAnchor.MiddleCenter;
+		style.font = font;
+		style.fontSize = 52;
+		if(GUI.Button(new Rect(Screen.width * (1f/6.55f),10*Screen.height * (0.2f/6.3f),Screen.width * (4.8f/6.55f),Screen.height * (0.85f/6.3f)),"Multiplayer", style)) {
 			Application.LoadLevel(1);
 		}
-		if(GUI.Button(new Rect(Screen.width * (1f/6.55f),10*Screen.height * (0.2f/6.3f),Screen.width * (4.8f/6.55f),Screen.height * (0.85f/6.3f)),"Exit")) {
+		if(GUI.Button(new Rect(Screen.width * (1f/6.55f),17*Screen.height * (0.2f/6.3f),Screen.width * (4.8f/6.55f),Screen.height * (0.85f/6.3f)),"Exit", style)) {
 			Application.Quit();
 		}
 		
