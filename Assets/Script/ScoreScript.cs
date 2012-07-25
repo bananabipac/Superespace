f19c67740fd1a35a0fb9f238d38a7dbe0b300325
PEDRA <romain.pedra@gmail.fr>
2012-07-25 10:55:34 +0200

    Couleurs upgrades

--- Assets/Script/ScoreScript.cs
+++ Assets/Script/ScoreScript.cs
@@ -5,6 +5,11 @@ public class ScoreScript : MonoBehaviour {
 	private string nameOfObject;
 	// Use this for initialization
 	void Start () {
+		if(PlayerPrefs.GetString("Sound") == "Muted"){
+			Camera.main.gameObject.GetComponent<AudioListener>().enabled = false;
+		}else{
+			Camera.main.gameObject.GetComponent<AudioListener>().enabled = true;
+		}
 		nameOfObject = gameObject.name;
 		if(nameOfObject == "timeGame") {
 			gameObject.guiText.text += PlayerPrefs.GetInt(nameOfObject+"Min")+":";
